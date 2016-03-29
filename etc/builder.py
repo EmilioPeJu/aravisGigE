c9e5120cc83d0b2c7862f9ae0668f0233f853acf
Tom Cobb <tom.cobb@diamond.ac.uk>
2016-03-29 15:25:26 +0100

    Added etc files for Photon focus

--- etc/builder.py
+++ etc/builder.py
@@ -7,7 +7,7 @@ from iocbuilder.modules.ADCore import ADCore, ADBaseTemplate, includesTemplates,
 @includesTemplates(ADBaseTemplate)
 class aravisCameraTemplate(AutoSubstitution):
     TemplateFile="aravisCamera.template"
-    
+
 class aravisCamera(AsynPort):
     '''Creates a aravisCamera camera areaDetector driver'''
     Dependencies = (ADCore,)
@@ -20,11 +20,11 @@ class aravisCamera(AsynPort):
         # Update the attributes of self from the commandline args
         self.__dict__.update(locals())
         # Make an instance of our template
-        makeTemplateInstance(self._SpecificTemplate, locals(), args)        
+        makeTemplateInstance(self._SpecificTemplate, locals(), args)
         # Backwards compatible Manta
         if CLASS == "AVT_Manta":
             CLASS += "_1_42"
-        # Init the camera specific class        
+        # Init the camera specific class
         class _tmp(AutoSubstitution):
             ModuleName = aravisCamera.ModuleName
             TrueName = "_%s" % CLASS
@@ -34,13 +34,14 @@ class aravisCamera(AsynPort):
     # __init__ arguments
     ArgInfo = _SpecificTemplate.ArgInfo + makeArgInfo(__init__,
         PORT    = Simple('Port name for the camera', str),
-        ID      = Simple('Cam ip address, MAC address, or ID <manufacturer>-<serial>, (e.g. Prosilica-02-2166A-06844)', str),    
-        CLASS   = Choice('Camera class for custom commands', 
+        ID      = Simple('Cam ip address, MAC address, or ID <manufacturer>-<serial>, (e.g. Prosilica-02-2166A-06844)', str),
+        CLASS   = Choice('Camera class for custom commands',
                          ["AVT_Manta_1_42", "AVT_Manta_1_44", "AVT_Manta_1_44_4",
-                          "AVT_Mako_1_52", "Prosilica_GC", "Baumer_TXG", 
+                          "AVT_Mako_1_52", "Prosilica_GC", "Baumer_TXG",
                           "JAI_6740", "JAI_CM140", "PGR_Flea3", "Sony_CVC_EH6300",
-                          "Baumer_HXG20", "PSL_SCMOS", "PGR_Blackfly_13E4C-CS", 
-                          "Basler_piA640_210gm", "SVS_evo4050"]),
+                          "Baumer_HXG20", "PSL_SCMOS", "PGR_Blackfly_13E4C-CS",
+                          "Basler_piA640_210gm", "SVS_evo4050",
+                          "Photonfocus_DR1_D2048x1088_192_G2"]),
         BUFFERS = Simple('Maximum number of NDArray buffers to be created for '
                          'plugin callbacks', int),
         MEMORY  = Simple('Max memory to allocate, should be maxw*maxh*nbuffer '
@@ -58,7 +59,7 @@ class aravisCamera(AsynPort):
 
 def aravisCamera_sim(aravisCamera=aravisCamera, **kwargs):
     kwargs.pop("ID")
-    kwargs.pop("CLASS")    
+    kwargs.pop("CLASS")
     return aravisCamera(ID="Fake_1", CLASS="Fake", **kwargs)
 
 SetSimulation(aravisCamera, aravisCamera_sim)
